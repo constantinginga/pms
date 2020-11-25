6b50eeab0da0c3c4b82d3d8a658612ce3b9362a4
Constantin Ginga <costelgn@gmail.com>
2020-11-25 09:09:42 +0100

    Format code

--- src/model/TeamMemberList.java
+++ src/model/TeamMemberList.java
@@ -4,63 +4,60 @@ import mediator.ProjectManagementSystem;
 
 import java.util.ArrayList;
 
-import java.util.ArrayList;
+public class TeamMemberList {
+    private ArrayList<TeamMember> teamMemberList;
 
-public class TeamMemberList
-{
-	private ArrayList<TeamMember> teamMemberList;
+    public TeamMemberList() {
+        this.teamMemberList = new ArrayList<>();
+    }
 
-	public TeamMemberList() { this.teamMemberList = new ArrayList<>(); }
+    public void add(TeamMember teamMember) {
+        teamMemberList.add(teamMember);
+    }
 
-	public void add(TeamMember teamMember) { teamMemberList.add(teamMember); }
+    public void remove(TeamMember teamMember) {
+        teamMemberList.remove(teamMember);
+    }
 
-	public void remove(TeamMember teamMember) { teamMemberList.remove(teamMember); }
+    public TeamMember findByName(String name) {
+        for (TeamMember team : teamMemberList) {
+            if (team.getName().equals(name)) return team;
+        }
 
-	public TeamMember findByName(String name)
-	{
-		for (TeamMember team : teamMemberList)
-		{
-			if(team.getName().equals(name)) return team;
-		}
-		return null;
-	}
+        return null;
+    }
 
-	public TeamMember findById(String Id)
-	{
-		for(TeamMember team : teamMemberList)
-		{
-			if(team.getId().equals(Id)) return team;
-		}
-		return null;
-	}
+    public TeamMember findById(String Id) {
+        for (TeamMember team : teamMemberList) {
+            if (team.getId().equals(Id)) return team;
+        }
 
-	public TeamMember getTeamMember(int index)
-	{
-		return teamMemberList.get(index);
-	}
+        return null;
+    }
 
-	public int getTeamSize()
-	{
-		return teamMemberList.size();
-	}
+    public TeamMember getTeamMember(int index) {
+        return teamMemberList.get(index);
+    }
 
-	public TeamMember getTeamMemberByName(String name)
-	{
-		for(TeamMember team : teamMemberList)
-		{
-			TeamMember temp = (TeamMember) team;
-			if(temp.getName().equals(name)) return temp;
-		}
-		return null;
-	}
+    public int getTeamSize() {
+        return teamMemberList.size();
+    }
 
-	public TeamMember getTeamMemberById(String id)
-	{
-		for(TeamMember team : teamMemberList)
-		{
-			TeamMember temp = (TeamMember) team;
-			if(temp.getId().equals(id)) return temp;
-		}
-		return null;
-	}
+    public TeamMember getTeamMemberByName(String name) {
+        for (TeamMember team : teamMemberList) {
+            TeamMember temp = (TeamMember) team;
+            if (temp.getName().equals(name)) return temp;
+        }
+
+        return null;
+    }
+
+    public TeamMember getTeamMemberById(String id) {
+        for (TeamMember team : teamMemberList) {
+            TeamMember temp = (TeamMember) team;
+            if (temp.getId().equals(id)) return temp;
+        }
+
+        return null;
+    }
 }
