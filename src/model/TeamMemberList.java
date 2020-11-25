085902fb76000551986c7dddb89d7d1588b14e37
gof3ck69 <sorryy647@gmail.com>
2020-11-25 09:02:56 +0100

    Team members

--- src/model/TeamMemberList.java
+++ src/model/TeamMemberList.java
@@ -4,40 +4,63 @@ import mediator.ProjectManagementSystem;
 
 import java.util.ArrayList;
 
-public class TeamMemberList {
+import java.util.ArrayList;
 
-	private ArrayList<TeamMember> teamMember;
-	private ProjectManagementSystem projectManagementSystem;
+public class TeamMemberList
+{
+	private ArrayList<TeamMember> teamMemberList;
 
-	public void add(TeamMember teammember) {
+	public TeamMemberList() { this.teamMemberList = new ArrayList<>(); }
 
-	}
+	public void add(TeamMember teamMember) { teamMemberList.add(teamMember); }
 
-	public void remove(TeamMember teammember) {
+	public void remove(TeamMember teamMember) { teamMemberList.remove(teamMember); }
 
-	}
-
-	public TeamMember findByName(String name) {
+	public TeamMember findByName(String name)
+	{
+		for (TeamMember team : teamMemberList)
+		{
+			if(team.getName().equals(name)) return team;
+		}
 		return null;
 	}
 
-	public TeamMember findById(String Id) {
+	public TeamMember findById(String Id)
+	{
+		for(TeamMember team : teamMemberList)
+		{
+			if(team.getId().equals(Id)) return team;
+		}
 		return null;
 	}
 
-	public TeamMemberList() {
-
+	public TeamMember getTeamMember(int index)
+	{
+		return teamMemberList.get(index);
 	}
 
-	public TeamMemberList getTeamMembers() {
+	public int getTeamSize()
+	{
+		return teamMemberList.size();
+	}
+
+	public TeamMember getTeamMemberByName(String name)
+	{
+		for(TeamMember team : teamMemberList)
+		{
+			TeamMember temp = (TeamMember) team;
+			if(temp.getName().equals(name)) return temp;
+		}
 		return null;
 	}
 
-	public int getTeamSize() {
-		return 0;
-	}
-	public String generateId() {
-
-
+	public TeamMember getTeamMemberById(String id)
+	{
+		for(TeamMember team : teamMemberList)
+		{
+			TeamMember temp = (TeamMember) team;
+			if(temp.getId().equals(id)) return temp;
+		}
+		return null;
 	}
 }
