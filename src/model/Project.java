8ca309f3e1b89a1cf5b8a8329dc5740b71d9057f
Jakub <jakub.platzek@gmail.com>
2020-12-04 14:06:21 +0100

    hhh

--- src/model/Project.java
+++ src/model/Project.java
@@ -2,7 +2,8 @@ package model;
 
 import java.util.ArrayList;
 
-public class Project extends GeneralTemplate {
+public class Project extends GeneralTemplate
+{
 
   private String id;
   private String title;
@@ -12,49 +13,112 @@ public class Project extends GeneralTemplate {
   private TeamMember productOwner;
   private RequirementList requirementList;
 
-  public Project(String title, String status) {
+  public Project(String title, String status)
+  {
     super(status);
-    this.title = title;
+    set(title);
     this.productOwner = null;
     this.scrumMaster = null;
     this.projectCreator = null;
     this.requirementList = new RequirementList();
   }
 
-  public void setId(String id) {
+  public void setId(String id)
+  {
+    try
+    {
+      int idInt = Integer.parseInt(id);
+    }
+    catch (NumberFormatException e)
+    {
+      throw new IllegalArgumentException("Invalid id");
+    }
     this.id = id;
   }
 
-  public void set(String title) {
+  public void set(String title)
+  {
+    if (title.equals("") || title == null || title.length() < 3)
+    {
+      throw new IllegalArgumentException("Invalid title");
+    }
     this.title = title;
   }
 
-  public void setNote() {
+  public void setNote()
+  {
+    try
+    {
+      int noteInt = Integer.parseInt(id);
+    }
+    catch (NumberFormatException e)
+    {
+      throw new IllegalArgumentException("Invalid id");
+    }
     this.note = note;
   }
 
-  public RequirementList getRequirementList() {
+  public RequirementList getRequirementList()
+  {
     return requirementList;
   }
 
-  public void setRequirementList(RequirementList requirementList) {
+  public void setRequirementList(RequirementList requirementList)
+  {
+    try
+    {
+      int reqInt = Integer.parseInt(id);
+    }
+    catch (NumberFormatException e)
+    {
+      throw new IllegalArgumentException("Invalid input");
+    }
     this.requirementList = requirementList;
   }
 
-  public void setProjectCreator(TeamMember teamMember) {
-		projectCreator = teamMember;
+  public void setProjectCreator(TeamMember teamMember)
+  {
+    try
+    {
+      int prCrInt = Integer.parseInt(id);
+    }
+    catch (NumberFormatException e)
+    {
+      throw new IllegalArgumentException("Invalid input");
+    }
+    projectCreator = teamMember;
   }
 
-  public void setScrumMaster(TeamMember teamMember) {
-		scrumMaster = teamMember;
+  public void setScrumMaster(TeamMember teamMember)
+  {
+    try
+    {
+      int scrMstrInt = Integer.parseInt(id);
+    }
+    catch (NumberFormatException e)
+    {
+      throw new IllegalArgumentException("Invalid input");
+    }
+    scrumMaster = teamMember;
   }
 
-  public void setProductOwner(TeamMember teamMember) {
-		productOwner = teamMember;
+  public void setProductOwner(TeamMember teamMember)
+  {
+    try
+    {
+      int prodOwnInt = Integer.parseInt(id);
+    }
+    catch (NumberFormatException e)
+    {
+      throw new IllegalArgumentException("Invalid input");
+    }
+    productOwner = teamMember;
   }
 
-  public void setPosition(String position, TeamMember teamMember) {
-    switch (position.toLowerCase()) {
+  public void setPosition(String position, TeamMember teamMember)
+  {
+    switch (position.toLowerCase())
+    {
       case "scrum master":
         scrumMaster = teamMember;
         break;
@@ -66,11 +130,13 @@ public class Project extends GeneralTemplate {
     }
   }
 
-  public String getId() {
-  	return id;
+  public String getId()
+  {
+    return id;
   }
 
-  public String getTitle() {
+  public String getTitle()
+  {
     return title;
   }
 }
