29f4dcadf62fe6df6d785516556fbaf1d6318ac2
Constantin Ginga <costelgn@gmail.com>
2020-12-08 13:11:37 +0100

    Add selection of team member roles

--- src/view/controller/AddProjectViewController.java
+++ src/view/controller/AddProjectViewController.java
@@ -1,5 +1,6 @@
 package view.controller;
 
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
@@ -9,94 +10,132 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 import mediator.ProjectManagementSystemModel;
-import model.MyDate;
-import model.Project;
-import model.Task;
-import model.TeamMember;
+import model.*;
 import view.ViewHandler;
 import view.ViewState;
 
 import java.time.LocalDate;
 
-public class AddProjectViewController
-{
-  private ViewHandler viewHandler;
-  private Region root;
-  private ProjectManagementSystemModel model;
-  private ViewState state;
-  @FXML private Label errorLabel;
-  @FXML private TextField titleTextField;
-  @FXML private ChoiceBox<String> teamMembersChoiceBox= new ChoiceBox<>();
-  @FXML private ChoiceBox<String> projectCreatorChoiceBox= new ChoiceBox<>();
-  @FXML private ChoiceBox<String> productOwnerChoiceBox= new ChoiceBox<>();
-  @FXML private ChoiceBox<String> scrumMasterChoiceBox= new ChoiceBox<>();
-  @FXML private ChoiceBox<String> statusChoiceBox= new ChoiceBox<>();
+public class AddProjectViewController {
+    private ViewHandler viewHandler;
+    private Region root;
+    private ProjectManagementSystemModel model;
+    private ViewState state;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private TextField noteTextField;
+    @FXML
+    private ChoiceBox<String> projectCreatorChoiceBox = new ChoiceBox<>();
+    @FXML
+    private ChoiceBox<String> productOwnerChoiceBox = new ChoiceBox<>();
+    @FXML
+    private ChoiceBox<String> scrumMasterChoiceBox = new ChoiceBox<>();
 
-  public AddProjectViewController()
-  {
-  }
-
-  public void init(ViewHandler viewHandler, Region root,
-                   ProjectManagementSystemModel model, ViewState state)
-  {
-    this.viewHandler = viewHandler;
-    this.root = root;
-    this.model = model;
-    this.state = state;
-
-  }
-
-  public void reset()
-  {
-    titleTextField.setText("");
-    teamMembersChoiceBox.valueProperty().setValue(null);
-    projectCreatorChoiceBox.valueProperty().setValue(null);
-    productOwnerChoiceBox.valueProperty().setValue(null);
-    scrumMasterChoiceBox.valueProperty().setValue(null);
-    statusChoiceBox.valueProperty().setValue(null);
-    errorLabel.setText("");
-  }
-  public Region getRoot()
-  {
-    return root;
-  }
-
-  @FXML private void handleAddButton()
-  {
-    if (titleTextField == null || titleTextField.getText().isEmpty()) {
-      errorLabel.setText("Please enter a title");
-      return;
+    public AddProjectViewController() {
     }
 
-    if (projectCreatorChoiceBox.getValue() == null) {
-      errorLabel.setText("Please select a project creator");
-      return;
+    public void init(ViewHandler viewHandler, Region root,
+                     ProjectManagementSystemModel model, ViewState state) {
+        this.viewHandler = viewHandler;
+        this.root = root;
+        this.model = model;
+        this.state = state;
+        setupChoiceBoxes();
     }
 
-    if (productOwnerChoiceBox == null) {
-      errorLabel.setText("Please enter a product owner");
-      return;
+    public void reset() {
+        titleTextField.setText("");
+        projectCreatorChoiceBox.valueProperty().setValue(null);
+        productOwnerChoiceBox.valueProperty().setValue(null);
+        scrumMasterChoiceBox.valueProperty().setValue(null);
+        noteTextField.setText("");
+        errorLabel.setText("");
     }
-    if (scrumMasterChoiceBox == null) {
-      errorLabel.setText("Please enter a scrum master");
-      return;
-    }
-    if (statusChoiceBox == null) {
-      errorLabel.setText("Please enter a status");
-      return;
-    }
-    Project newProject = new Project(titleTextField.getText(), statusChoiceBox.getAccessibleText());
-    newProject.setId(state.getSelectedTaskID());
-    model.addProject(newProject);
-    viewHandler.openView("mainWindow");
-  }
 
-  @FXML private void handleCancelButton()
-  {
-    viewHandler.openView("mainWindow");
-  }
+    public Region getRoot() {
+        return root;
+    }
 
-  public void handleAddTeamMemberButton(ActionEvent actionEvent)
-  {
-  }
+    private void setupChoiceBoxes() {
+        for (int i = 0; i < model.getTeamMemberList().getSize(); i++) {
+            projectCreatorChoiceBox.getItems().add(model.getTeamMemberList().getTeamMember(i).getName());
+            productOwnerChoiceBox.getItems().add(model.getTeamMemberList().getTeamMember(i).getName());
+            scrumMasterChoiceBox.getItems().add(model.getTeamMemberList().getTeamMember(i).getName());
+        }
+
+        // prevents the selection of the same member for more
+        projectCreatorChoiceBox.getSelectionModel().selectedItemProperty()
+                .addListener((ObservableValue<? extends String> ov, String old_val, String new_val) -> {
+                    if (!new_val.equals(old_val)) {
+                        productOwnerChoiceBox.getItems().remove(new_val);
+                        scrumMasterChoiceBox.getItems().remove(new_val);
+                        if (old_val != null) {
+                            productOwnerChoiceBox.getItems().add(old_val);
+                            scrumMasterChoiceBox.getItems().add(old_val);
+                        }
+                    }
+                });
+
+        productOwnerChoiceBox.getSelectionModel().selectedItemProperty()
+                .addListener((ObservableValue<? extends String> ov, String old_val, String new_val) -> {
+                    if (!new_val.equals(old_val)) {
+                        projectCreatorChoiceBox.getItems().remove(new_val);
+                        scrumMasterChoiceBox.getItems().remove(new_val);
+                        if (old_val != null) {
+                            projectCreatorChoiceBox.getItems().add(old_val);
+                            scrumMasterChoiceBox.getItems().add(old_val);
+                        }
+                    }
+                });
+
+        scrumMasterChoiceBox.getSelectionModel().selectedItemProperty()
+                .addListener((ObservableValue<? extends String> ov, String old_val, String new_val) -> {
+                    if (!new_val.equals(old_val)) {
+                        projectCreatorChoiceBox.getItems().remove(new_val);
+                        productOwnerChoiceBox.getItems().remove(new_val);
+                        if (old_val != null) {
+                            projectCreatorChoiceBox.getItems().add(old_val);
+                            productOwnerChoiceBox.getItems().add(old_val);
+                        }
+                    }
+                });
+    }
+
+    @FXML
+    private void handleAddButton() {
+        if (titleTextField == null || titleTextField.getText().isEmpty()) {
+            errorLabel.setText("Please enter a title");
+            return;
+        }
+
+        if (projectCreatorChoiceBox.getValue() == null) {
+            errorLabel.setText("Please select a project creator");
+            return;
+        }
+
+        if (productOwnerChoiceBox == null) {
+            errorLabel.setText("Please enter a product owner");
+            return;
+        }
+        if (scrumMasterChoiceBox == null) {
+            errorLabel.setText("Please enter a scrum master");
+            return;
+        }
+        Project newProject = new Project(titleTextField.getText(), GeneralTemplate.STATUS_NOT_STARTED);
+        newProject.setId(state.getSelectedTaskID());
+        if (noteTextField.getText() != null) newProject.setNote(noteTextField.getText());
+        model.addProject(newProject);
+        viewHandler.openView("mainWindow");
+    }
+
+    @FXML
+    private void handleCancelButton() {
+        viewHandler.openView("mainWindow");
+    }
+
+    public void handleAddTeamMemberButton(ActionEvent actionEvent) {
+    }
 }
