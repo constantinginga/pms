2064d6ddff8766dc7574ee78cb229ad84520ca51
Constantin Ginga <costelgn@gmail.com>
2020-12-09 09:52:15 +0100

    Implement edit method

--- src/view/controller/TeamMemberListViewController.java
+++ src/view/controller/TeamMemberListViewController.java
@@ -1,10 +1,7 @@
 package view.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Region;
 import mediator.ProjectManagementSystemModel;
 import model.TeamMember;
@@ -14,46 +11,56 @@ import view.viewModel.TeamMemberListViewModel;
 import view.viewModel.TeamMemberViewModel;
 
 
-
 public class TeamMemberListViewController {
-    @FXML private TextField nameTextField;
-    @FXML private Label errorLabel;
-    @FXML private TableView<TeamMemberViewModel> teamMemberListTable;
-    @FXML private TableColumn<TeamMemberViewModel, String> idColumn;
-    @FXML private TableColumn<TeamMemberViewModel, String> nameColumn;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private Button editButton;
+    @FXML
+    private TableView<TeamMemberViewModel> teamMemberListTable;
+    @FXML
+    private TableColumn<TeamMemberViewModel, String> idColumn;
+    @FXML
+    private TableColumn<TeamMemberViewModel, String> nameColumn;
     private TeamMemberListViewModel teamMemberListViewModel;
+    private TeamMemberViewModel selected;
     private ProjectManagementSystemModel model;
     private ViewHandler viewHandler;
     private Region root;
     private ViewState state;
 
-    public TeamMemberListViewController(){
+    public TeamMemberListViewController() {
 
     }
 
-    public void init(ViewHandler viewHandler, Region root,ProjectManagementSystemModel model, ViewState state){
+    public void init(ViewHandler viewHandler, Region root, ProjectManagementSystemModel model, ViewState state) {
         this.viewHandler = viewHandler;
         this.model = model;
         this.root = root;
         this.state = state;
         this.teamMemberListViewModel = new TeamMemberListViewModel(model);
+        this.selected = null;
         idColumn.setCellValueFactory(cellData -> cellData.getValue().idPropertyProperty());
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().namePropertyProperty());
         teamMemberListTable.setItems(teamMemberListViewModel.getList());
     }
 
-    public void reset(){
+    public void reset() {
         nameTextField.setText("");
         errorLabel.setText("");
         teamMemberListViewModel.update();
+        editButton.setText("Edit");
     }
 
-    public Region getRoot(){
+    public Region getRoot() {
         return root;
     }
 
-    @FXML private void handleAddButton(){
-        if (nameTextField.getText() == null || nameTextField.getText().equals("")){
+    @FXML
+    private void handleAddButton() {
+        if (nameTextField.getText() == null || nameTextField.getText().equals("")) {
             errorLabel.setText("Name cannot be empty");
             return;
         }
@@ -68,33 +75,51 @@ public class TeamMemberListViewController {
 
 
     @FXML
-    private void handleRemoveButton(){
+    private void handleRemoveButton() {
         errorLabel.setText("");
-        try{
+        try {
             TeamMemberViewModel selected = teamMemberListTable.getSelectionModel().getSelectedItem();
             TeamMember teamMember = new TeamMember(selected.getNameProperty());
             teamMember.setId(selected.getIdProperty());
             model.removeTeamMember(teamMember);
             teamMemberListViewModel.remove(teamMember);
             teamMemberListTable.getSelectionModel().clearSelection();
-        } catch (Exception e){
+        } catch (Exception e) {
             errorLabel.setText("Item not found: " + e.getMessage());
         }
     }
 
-   @FXML
-   private void handleEditButton(){
-       errorLabel.setText("");
-       try{
-           TeamMemberViewModel selected = teamMemberListTable.getSelectionModel().getSelectedItem();
-           nameTextField.setText(selected.getNameProperty());
-       } catch(Exception e){
-           e.printStackTrace();
-       }
+    @FXML
+    private void handleEditButton() {
+        errorLabel.setText("");
+        if (editButton.getText().equals("Edit")) {
+            try {
+                selected = teamMemberListTable.getSelectionModel().getSelectedItem();
+                nameTextField.setText(selected.getNameProperty());
+                editButton.setText("Save");
+            } catch (Exception e) {
+                errorLabel.setText("Please select a team member");
+            }
+        } else {
+            if (nameTextField.getText() == null || nameTextField.getText().equals("")) {
+                errorLabel.setText("Please enter a new name");
+                return;
+            }
+
+            if (selected != null && !nameTextField.getText().equals(selected.getNameProperty())) {
+                TeamMember selectedTeamMember = model.getTeamMemberList().findByName(selected.getNameProperty());
+                selectedTeamMember.setName(nameTextField.getText());
+                teamMemberListViewModel.changeMember(selectedTeamMember);
+                teamMemberListViewModel.update();
+                teamMemberListTable.getSelectionModel().clearSelection();
+                nameTextField.clear();
+                editButton.setText("Edit");
+            }
+        }
     }
 
     @FXML
-    private void handleCancelButton(){
+    private void handleCancelButton() {
         viewHandler.openView("mainWindow");
     }
 
