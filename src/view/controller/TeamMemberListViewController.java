08c7dbc81f8b008252a456a7619a540486ba98da
Constantin Ginga <costelgn@gmail.com>
2020-12-12 15:31:42 +0100

    Update methods and clean up code

--- src/view/controller/TeamMemberListViewController.java
+++ src/view/controller/TeamMemberListViewController.java
@@ -39,12 +39,15 @@ public class TeamMemberListViewController {
 
     public void init(ViewHandler viewHandler, Region root, ProjectManagementSystemModel model, ViewState state) {
         this.viewHandler = viewHandler;
-        this.model = model;
         this.root = root;
+        this.model = model;
         this.state = state;
-
         this.teamMemberListViewModel = new TeamMemberListViewModel(model);
         this.selected = null;
+        initTable();
+    }
+
+    public void initTable() {
         idColumn.setCellValueFactory(cellData -> cellData.getValue().idPropertyProperty());
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().namePropertyProperty());
         teamMemberListTable.setItems(teamMemberListViewModel.getList());
@@ -61,17 +64,15 @@ public class TeamMemberListViewController {
         return root;
     }
 
-    private boolean confirmation()
-    {
+    private boolean confirmation() {
         int index = teamMemberListTable.getSelectionModel().getSelectedIndex();
-        TeamMemberViewModel selectedItem = teamMemberListTable.getItems()
-                .get(index);
-        if (index < 0 || index >= teamMemberListTable.getItems().size())
-        {
-            return false;
-        }
+        TeamMemberViewModel selectedItem = teamMemberListTable.getItems().get(index);
+
+        if (index >= teamMemberListTable.getItems().size()) return false;
+        // create alert
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
+        // set alert message
         alert.setHeaderText(
                 "Removing team member [" + selectedItem.getIdProperty() + "] "
                         + selectedItem.getNameProperty());
@@ -81,45 +82,69 @@ public class TeamMemberListViewController {
 
     @FXML
     private void handleAddButton() {
+        // check for empty name
         if (nameTextField.getText() == null || nameTextField.getText().equals("")) {
-            errorLabel.setText("Name cannot be empty");
+            errorLabel.setText("Please enter a name");
             return;
         }
-        TeamMember other = new TeamMember(nameTextField.getText());
+
+        // prevent user from adding a new team member when he's already editing an existing one
+        if (editButton.getText().equals("Save")) {
+            errorLabel.setText("Please save your changes first");
+            return;
+        }
+
+        // check for valid name
+        TeamMember other = null;
+        try {
+            other = new TeamMember(nameTextField.getText());
+        } catch (IllegalArgumentException e) {
+            errorLabel.setText(e.getMessage());
+            return;
+        }
+
+        // determine the id of the newly created TeamMember according to table size
         int position = (teamMemberListTable.getItems().size() == 0) ? 0 : teamMemberListTable.getItems().size() - 1;
         other.setId(Integer.toString(position));
+        // add new TeamMember to model and reset
         model.addTeamMember(other);
-        errorLabel.setText("");
-        nameTextField.setText("");
-        teamMemberListViewModel.update();
+        reset();
     }
 
 
-    @FXML private void handleRemoveButton() {
+    @FXML
+    private void handleRemoveButton() {
         errorLabel.setText("");
+
+        if (editButton.getText().equals("Save")) {
+            errorLabel.setText("Please save your changes first");
+            return;
+        }
+
         try {
             // selection of teamMember to remove
             TeamMemberViewModel selected = teamMemberListTable.getSelectionModel()
                     .getSelectedItem();
             TeamMember teamMember = new TeamMember(selected.getNameProperty());
             teamMember.setId(selected.getIdProperty());
+
             // removing from list
             boolean remove = confirmation();
             if (remove) {
                 model.removeTeamMember(teamMember);
                 teamMemberListViewModel.remove(teamMember);
-                teamMemberListTable.getSelectionModel().clearSelection();
+//                teamMemberListTable.getSelectionModel().clearSelection();
             }
-
         } catch (Exception e) {
-            errorLabel.setText("Choose team member to remove");
+            errorLabel.setText("Select team member to remove");
         }
     }
 
-        @FXML
+    @FXML
     private void handleEditButton() {
         errorLabel.setText("");
         if (editButton.getText().equals("Edit")) {
+            // get selected TeamMember
             try {
                 selected = teamMemberListTable.getSelectionModel().getSelectedItem();
                 nameTextField.setText(selected.getNameProperty());
@@ -128,26 +153,35 @@ public class TeamMemberListViewController {
                 errorLabel.setText("Please select a team member");
             }
         } else {
+            // if nameTextField is left empty
             if (nameTextField.getText() == null || nameTextField.getText().equals("")) {
                 errorLabel.setText("Please enter a new name");
                 return;
             }
 
+            // if no changes were made, reset
+            if (nameTextField.getText().equals(selected.getNameProperty())) {
+                reset();
+                return;
+            }
+
+            // if name was changed, add to model and reset
             if (selected != null && !nameTextField.getText().equals(selected.getNameProperty())) {
                 TeamMember selectedTeamMember = model.getTeamMemberList().findByName(selected.getNameProperty());
-                selectedTeamMember.setName(nameTextField.getText());
+                try {
+                    selectedTeamMember.setName(nameTextField.getText());
+                } catch (IllegalArgumentException e) {
+                    errorLabel.setText(e.getMessage());
+                    return;
+                }
                 teamMemberListViewModel.changeMember(selectedTeamMember);
-                teamMemberListViewModel.update();
-                teamMemberListTable.getSelectionModel().clearSelection();
-                nameTextField.clear();
-                editButton.setText("Edit");
+                reset();
             }
         }
     }
 
     @FXML
-    private void handleCancelButton() {
+    private void handleBackButton() {
         viewHandler.openView("mainWindow");
     }
-
 }
