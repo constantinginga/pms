66d1843b78ff2d72b1acd7de1630c267aaf28a9e
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2020-12-15 12:46:29 +0100

    added logo

--- src/ProjectManagementApplication.java
+++ src/ProjectManagementApplication.java
@@ -1,8 +1,11 @@
 import com.sun.javafx.css.StyleManager;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import mediator.ProjectManagementSystemModelManager;
 import view.ViewHandler;
+
+
 
 public class ProjectManagementApplication extends Application {
 
@@ -10,6 +13,8 @@ public class ProjectManagementApplication extends Application {
     public void start(Stage stage) {
         ViewHandler viewHandler = new ViewHandler(new ProjectManagementSystemModelManager());
         StyleManager.getInstance().addUserAgentStylesheet(viewHandler.getCSS());
+        Image image = new Image("logo/logopms.png");
+        stage.getIcons().add(image);
         Application.setUserAgentStylesheet(viewHandler.getCSS());
         viewHandler.start(stage);
     }
